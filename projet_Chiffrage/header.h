7fe37fc12070e3ac19aaf659f0de7fc260209cda
leonancelin <l.ancelin16@ejm.org>
2023-08-29 09:33:09 +0200

    tuesday 9:30

--- projet_Chiffrage/header.h
+++ projet_Chiffrage/header.h
@@ -1,24 +1,33 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
-/*
+
 char rallongement_perroquet(char texte[], char cle[])
 {
-    int size_texte = sizeof(texte);
-    int size_cle = sizeof(cle);
+    int size_texte = strlen(texte);
+    int size_cle = strlen(cle);
+    bool taille = false;
 
     if(size_cle%size_texte < 1)
+        taille = true;
+
+    while(taille)
     {
         char temp_cle[size_cle];
         strcpy(temp_cle, cle);
         strcat(cle, temp_cle);
+        if(size_cle%size_texte < 1)
+            taille = true;
+
+        else
+            taille = false;
 
     }
 
     return texte;
     return cle;
 
-} */
+}
 
 
 
@@ -26,6 +35,7 @@ char chiffrage(char texte[], char cle[], char texte_chiffre[])
 {
     int size_texte = strlen(texte);
     int size_cle = strlen(cle);
+    int iteration = 0;
 
     //convert string to ASCII values
 
@@ -51,6 +61,12 @@ char chiffrage(char texte[], char cle[], char texte_chiffre[])
     for (int i=0; i<size_texte; i++)
     {
         ascii_chiffrage[i] = ascii_texte[i] - ascii_cle[i];
+        if(ascii_cle[i] == "\0")
+        {
+            n += 1;
+            ascii_chiffrage[i] = ascii_texte[i] - ascii_cle[i-n*size_cle];
+
+        }
 
     }
 
--- projet_Chiffrage/main.c
+++ projet_Chiffrage/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "header.h"
 
-//#include "chiffrage.c"
 
 char texte[200];
 char cle[200];
